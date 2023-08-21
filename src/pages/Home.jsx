e697499c7cece138b8a03b9cfe4492c3fdd4788f
Moulishwaran <moulish0809@gmail.com>
2023-08-21 22:33:30 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,171 @@
+import React from "react";
+import "../styles/home.css";
+
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../assets/images/hero-img01.jpg";
+import heroImg02 from "../assets/images/hero-img02.jpg";
+import heroVideo from "../assets/images/hero-video.mp4";
+import worldImg from "../assets/images/world.png";
+import experienceImg from "../assets/images/experience.png";
+import Subtitle from "../shared/Subtitle";
+import SearchBar from "../shared/SearchBar";
+import ServiceList from "../services/ServiceList";
+import FeatureToueList from "../components/Featured-tours/FeatureToueList";
+import MasonryImageGallery from "../components/images-gallery/MasonryImageGallery";
+import Testimonial from "../components/Testimonial/Testimonial";
+import Newsletter from "../shared/Newsletter";
+
+const Home = () => {
+  return (
+    <>
+      {/* ======== hero section start=========== */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="hero__content">
+                <div className="hero__subtitle d-flex align-items-center">
+                  <Subtitle subtitle={"Know Before You Go"} />
+                  <img src={worldImg} alt="" />
+                </div>
+                <h1>
+                  Traveling opens the door to creating{" "}
+                  <span className="highlight">memories</span>
+                </h1>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+                  illum alias, temporibus ipsam cupiditate vero, voluptatibus,
+                  facere praesentium labore aliquid ut modi ipsa eum delectus
+                  nostrum atque! Atque, numquam aliquam.
+                </p>
+              </div>
+            </Col>
+
+            <Col lg="2">
+              <div className="hero__img-box">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box hero__video-box mt-4">
+                <video src={heroVideo} alt="" controls loop />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-5">
+                <img src={heroImg02} alt="" />
+              </div>
+            </Col>
+
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+      {/*========hero section satrt============*/}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3">
+              <h5 className="service__subtitle">What we serve</h5>
+              <h2 className="service__title">We offer our best services</h2>
+            </Col>
+            <ServiceList />
+          </Row>
+        </Container>
+      </section>
+
+      {/* ==========featured tour section start========= */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="120" className="mb-5">
+              <Subtitle subtitle={"Explore"} />
+              <h2 className="featured__tour-title">Our Featured Tours</h2>
+            </Col>
+            <FeatureToueList />
+          </Row>
+        </Container>
+      </section>
+
+      {/* ==========featured tour section end========= */}
+
+      {/* =============experience section start============ */}
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="experience__Content">
+                <Subtitle subtitle={"Expericence"} />
+
+                <h2>
+                  With our all experience <br /> we will serve you
+                </h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  <br />
+                  nobis sunt ullam pariatur repellendus ab.
+                </p>
+              </div>
+              <div className="counter__wrapper d-flex align-items-center gap-5">
+                <div className="counter__box">
+                  <span>12k+</span>
+                  <h6>Succesful Trip</h6>
+                </div>
+                <div className="counter__box">
+                  <span>2k+</span>
+                  <h6>Regular Clients</h6>
+                </div>
+                <div className="counter__box">
+                  <span>15+</span>
+                  <h6>Years Experience</h6>
+                </div>
+              </div>
+            </Col>
+            <Col lg="6">
+              <div className="experience__img">
+                <img src={experienceImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      {/* =============experience section end============ */}
+
+      {/*========= Gallery section start==============*/}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              <Subtitle subtitle={"Gallery"} />
+              <h2 className="gallery__title">
+                Visit our customers tour Gallery
+              </h2>
+            </Col>
+            <Col lg="12">
+              <MasonryImageGallery />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      {/*========= Gallery section end==============*/}
+
+      {/*==========Testimonial section start */}
+      <section>
+        <Container>
+          <Col lg="12">
+            <Subtitle subtitle={"Fans Love"} />
+            <h2 className="testimonial__title">What our fans say about us</h2>
+          </Col>
+          <Col lg="12">
+            <Testimonial />
+          </Col>
+        </Container>
+      </section>
+      {/*==========Testimonial section end=================== */}
+      <Newsletter />
+    </>
+  );
+};
+
+export default Home;
---
+++ src/utils/config.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "https://tour-management-app-o653.onrender.com/api/v1";
+
+//("https://tour-management-app-o653.onrender.com");
